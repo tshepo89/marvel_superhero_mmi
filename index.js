23df409218c5636b62b67bfef7c4d7430c7d8be1
Tshepo Morailane <morailanetshepo@gmail.com>
2018-09-18 08:46:43 +0200

    modified headers

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const http = require('http')
+const express = require('express')
+const app = express()
 const md5 = require('md5')
 
 const times_stamp = '1'
@@ -10,8 +12,30 @@ let hash = md5(times_stamp + private_key+ public_key)
 var options = {
   host: 'gateway.marvel.com',
   path: 'https://gateway.marvel.com/v1/public/characters?ts=1&apikey='+public_key+'&hash='+hash,
-  headers: {
+  setHeaders: {
       'Referer': 'developer.marvel.com',
   }
 };
 
+app.use(express.static('public', options))
+app.get('')
+
+
+
+var req = http.get(options, function(res) {
+  // console.log('STATUS: ' + res.statusCode);
+  // console.log('HEADERS: ' + JSON.stringify(res.headers));
+  // res.setEncoding('utf8');
+  res.on('data', function (chunk) {
+    console.log('BODY: ' + chunk);
+  });
+});
+
+// req.on('error', function(e) {
+//   console.log('problem with request: ' + e.message);
+// });
+
+// write data to request body
+// req.write('data\n');
+// req.write('data\n');
+req.end();
