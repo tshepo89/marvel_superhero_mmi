5cec3b1703aec95d5110586abe867a23de4b8a88
Tshepo Morailane <morailanetshepo@gmail.com>
2018-09-19 01:48:27 +0200

    installed request js

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
-const http = require('http')
-const express = require('express')
-const app = express()
+const request = require('request')
 const md5 = require('md5')
 
 const times_stamp = '1'
@@ -9,27 +7,28 @@ const public_key = '3187ef773dc8ab9f494de9798eec872d'
 
 let hash = md5(times_stamp + private_key+ public_key)
 
-var options = {
-  host: 'gateway.marvel.com',
-  path: 'https://gateway.marvel.com/v1/public/characters?ts=1&apikey='+public_key+'&hash='+hash,
-  setHeaders: {
-      'Referer': 'developer.marvel.com',
-  }
-};
+request('https://gateway.marvel.com/v1/public/characters?ts=1&apikey='+public_key+'&hash='+hash, function(err, res, body){
+  console.log(body)
+})
 
-app.use(express.static('public', options))
-app.get('')
+// var options = {
+//   host: 'gateway.marvel.com',
+//   path: 'https://gateway.marvel.com/v1/public/characters?ts=1&apikey='+public_key+'&hash='+hash,
+//   headers: {
+//       'Referer': 'developer.marvel.com',
+//   }
+// };
 
 
 
-var req = http.get(options, function(res) {
-  // console.log('STATUS: ' + res.statusCode);
-  // console.log('HEADERS: ' + JSON.stringify(res.headers));
-  // res.setEncoding('utf8');
-  res.on('data', function (chunk) {
-    console.log('BODY: ' + chunk);
-  });
-});
+// var req = http.get(options, function(res) {
+//   // console.log('STATUS: ' + res.statusCode);
+//   // console.log('HEADERS: ' + JSON.stringify(res.headers));
+//   // res.setEncoding('utf8');
+//   res.on('data', function (chunk) {
+//     console.log('BODY: ' + chunk);
+//   });
+// });
 
 // req.on('error', function(e) {
 //   console.log('problem with request: ' + e.message);
@@ -38,4 +37,4 @@ var req = http.get(options, function(res) {
 // write data to request body
 // req.write('data\n');
 // req.write('data\n');
-req.end();
+// req.end();
